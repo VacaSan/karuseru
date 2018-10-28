6e5a3d3bbbaca03f578cd593dfddb1abbb709a3e
VacaSan <kotaracvladan@gmail.com>
2018-10-28 17:41:48 +0100

    Basic props types.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,44 @@
-import React, { Component } from "react";
-import "./SimpleCarousel.css";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+import './SimpleCarousel.css';
 
+/**
+ * SimpleCarousel component.
+ */
 class SimpleCarousel extends Component {
+  /**
+   * @type {object}
+   */
+  static get propTypes() {
+    return {
+      className: PropTypes.string,
+      style: PropTypes.object,
+    };
+  }
+
+  /**
+   * @type {Object}
+   */
+  static get defaultProps() {
+    return {
+      className: '',
+      style: {},
+    };
+  }
+
   render() {
-    return <div className="SimpleCarousel">hello, world</div>;
+    const { className, style, ...props } = this.props;
+
+    return (
+      <div
+        className={cx(SimpleCarousel, { [className]: className })}
+        style={style}
+        {...props}
+      >
+        hello, world
+      </div>
+    );
   }
 }
 
