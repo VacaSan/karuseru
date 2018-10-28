f73b6b8723b0f313898bda7fb228810b133f1b9f
VacaSan <kotaracvladan@gmail.com>
2018-10-28 19:08:20 +0100

    Control API check.

--- examples/src/index.js
+++ examples/src/index.js
@@ -1,20 +1,38 @@
-import React from "react";
+import React, { Component, createRef } from "react";
 import { render } from "react-dom";
 import SimpleCarousel from "../../src";
 import "./styles.css";
 
-const App = () => (
-  <div style={{
-    width: '100%',
-    maxWidth: 640,
-    margin: '0 auto',
-  }}>
-    <SimpleCarousel>
-      {[1, 2, 3].map((a) => (
-        <div key={a} className="slide">Slide {a}</div>
-      ))}
-    </SimpleCarousel>
-  </div>
-);
+class App extends Component {
+  carousel = createRef();
+
+  next = () => {
+    this.carousel.current.next();
+  }
+
+  prev = () => {
+    this.carousel.current.prev();
+  }
+
+  render() {
+    return (
+      <div style={{
+        width: '100%',
+        maxWidth: 640,
+        margin: '0 auto',
+      }}>
+        <SimpleCarousel ref={this.carousel}>
+          {[1, 2, 3].map((a) => (
+            <div key={a} className="slide">Slide {a}</div>
+          ))}
+        </SimpleCarousel>
+        <div>
+          <button onClick={this.prev}>&lt;</button>
+          <button onClick={this.next}>&gt;</button>
+        </div>
+      </div>
+    );
+  }
+}
 
 render(<App />, document.getElementById("root"));
