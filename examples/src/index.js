eb93a1848c0600b356561c69e5def00758dd91c2
VKotarac <vladan.kotarac@odt.net>
2018-10-28 14:17:44 +0100

    Initial commit.

---
+++ examples/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { render } from "react-dom";
+import Test from "../../src";
+
+const App = () => <Test />;
+
+render(<App />, document.getElementById("root"));
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Test.css";
+
+const Test = () => <div className="Test">hello, world</div>;
+
+export default Test;
