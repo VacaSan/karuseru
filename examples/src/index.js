3dc8d435e4d9fdb3834fce3f7d109680893156d5
VacaSan <kotaracvladan@gmail.com>
2019-03-11 15:56:38 +0100

    update demo

--- examples/src/index.js
+++ examples/src/index.js
@@ -4,48 +4,34 @@ import Karuseru from "../../src";
 import "./styles.css";
 
 const imgs = [
-  'https://images.pexels.com/photos/5439/earth-space.jpg?auto=compress&cs=tinysrgb&dpr=2&h=350',
-  'https://images.pexels.com/photos/1434608/pexels-photo-1434608.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=350',
-  'https://images.pexels.com/photos/733475/pexels-photo-733475.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=350',
+  "https://images.pexels.com/photos/5439/earth-space.jpg?auto=compress&cs=tinysrgb&dpr=2&h=350",
+  "https://images.pexels.com/photos/1434608/pexels-photo-1434608.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=350",
+  "https://images.pexels.com/photos/733475/pexels-photo-733475.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=350"
 ];
 
 class App extends Component {
   carousel = createRef();
 
-  state = {
-    slide: 0,
-    hasNext: true,
-    hasPrev: false,
+  next = () => {
+    this.carousel.current.nextSlide();
   };
 
-  next = () => {
-    this.carousel.current.next();
-  }
-
   prev = () => {
-    this.carousel.current.prev();
-  }
-
-  onChange = ({ slide, length }) => {
-    this.setState({
-      slide,
-      hasNext: slide < length - 1,
-      hasPrev: slide > 0,
-    });
-  }
+    this.carousel.current.prevSlide();
+  };
 
   render() {
-    const { slide, hasNext, hasPrev } = this.state;
-
     return (
-      <div style={{
-        width: '100%',
-        maxWidth: 640,
-        margin: '0 auto',
-      }}>
+      <div
+        style={{
+          width: "100%",
+          maxWidth: 640,
+          margin: "0 auto"
+        }}
+      >
         <div className="wrap">
-          <Karuseru ref={this.carousel} slide={slide} onChange={this.onChange}>
-            {imgs.map((src) => (
+          <Karuseru ref={this.carousel}>
+            {imgs.map(src => (
               <div key={src} className="slide">
                 <div className="card">
                   <div className="card_media img">
@@ -60,8 +46,8 @@ class App extends Component {
           </Karuseru>
         </div>
         <div>
-          <button onClick={this.prev} disabled={!hasPrev}>&lt;</button>
-          <button onClick={this.next} disabled={!hasNext}>&gt;</button>
+          <button onClick={this.prev}>&lt;</button>
+          <button onClick={this.next}>&gt;</button>
         </div>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import React, { Component, createRef, cloneElement } from 'react';
-import PropTypes from 'prop-types';
-import { sum, clamp, classnames, map } from './utils';
-import './Karuseru.css';
+import React, { Component, createRef, cloneElement } from "react";
+import PropTypes from "prop-types";
+import { sum, clamp, classnames, map } from "./utils";
+import KaruseruContainer from "./Container";
+import "./Karuseru.css";
 
 /**
  * Karuseru: a simple react carousel component.
@@ -12,7 +13,7 @@ class Karuseru extends Component {
     totalWidth: 0,
     childrenWidths: [],
     delta: 0,
-    isTouching: false,
+    isTouching: false
   };
 
   $root = createRef();
@@ -31,8 +32,8 @@ class Karuseru extends Component {
       settings: PropTypes.shape({
         duration: PropTypes.number, // in ms,
         easing: PropTypes.string, // timing function,
-        delay: PropTypes.number, // in ms,
-      }),
+        delay: PropTypes.number // in ms,
+      })
     };
   }
 
@@ -42,9 +43,9 @@ class Karuseru extends Component {
   static get defaultProps() {
     return {
       disabled: false,
-      className: '',
+      className: "",
       style: {},
-      settings: Karuseru.SETTINGS,
+      settings: Karuseru.SETTINGS
     };
   }
 
@@ -54,8 +55,8 @@ class Karuseru extends Component {
   static get SETTINGS() {
     return {
       duration: 225,
-      easing: 'cubic-bezier(0.0, 0.0, 0.2, 1)',
-      delay: 0,
+      easing: "cubic-bezier(0.0, 0.0, 0.2, 1)",
+      delay: 0
     };
   }
 
@@ -101,28 +102,28 @@ class Karuseru extends Component {
       this.goTo(slide);
     });
 
-    window.addEventListener('resize', this.onResize);
+    window.addEventListener("resize", this.onResize);
   }
 
   componentWillUnmount() {
-    window.removeEventListener('resize', this.onResize);
+    window.removeEventListener("resize", this.onResize);
   }
 
   /**
    * click event handler.
    */
-  onClick = (evt) => {
+  onClick = evt => {
     const { delta } = this.state;
 
     if (delta !== 0) {
       evt.preventDefault();
     }
-  }
+  };
 
   /**
    * mousedown/touchstart event handler.
    */
-  onStart = (evt) => {
+  onStart = evt => {
     evt.preventDefault();
 
     this._delta = 0;
@@ -131,16 +132,16 @@ class Karuseru extends Component {
     window.requestAnimationFrame(this.update);
 
     this.setState({
-      isTouching: true,
+      isTouching: true
     });
-  }
+  };
 
   /**
    * mousemove/touchmove event handler.
    */
-  onMove = (evt) => {
+  onMove = evt => {
     this._delta = (evt.pageX || evt.touches[0].pageX) - this.startX;
-  }
+  };
 
   /**
    * mouseup/touchend/touchcancel event handler.
@@ -149,9 +150,9 @@ class Karuseru extends Component {
     this.removeEventListeners();
 
     this.setState({
-      isTouching: false,
+      isTouching: false
     });
-  }
+  };
 
   /**
    * resize event handler. Runs layout after resize event has ended.
@@ -162,29 +163,32 @@ class Karuseru extends Component {
     this.resizeTimer = setTimeout(() => {
       this.layout();
     }, 250);
-  }
+  };
 
   /**
    * Recomputes the dimensions and re-lays out the component.
    *
    * @param {Function} [callback] Callback to run after the layout calculations.
    */
-  layout = (callback) => {
+  layout = callback => {
     const $root = this.$root.current;
     const { width } = $root.getBoundingClientRect();
 
-    const childrenWidths = map($root.children, (child) => {
+    const childrenWidths = map($root.children, child => {
       return child.getBoundingClientRect().width;
     });
 
     const totalWidth = childrenWidths.reduce(sum, 0);
 
-    this.setState({
-      width,
-      totalWidth,
-      childrenWidths,
-    }, callback);
-  }
+    this.setState(
+      {
+        width,
+        totalWidth,
+        childrenWidths
+      },
+      callback
+    );
+  };
 
   /**
    * Updates the state based on the user gesture.
@@ -194,7 +198,7 @@ class Karuseru extends Component {
     const { slide } = this.props;
 
     this.setState({
-      delta: isTouching ? this._delta : 0,
+      delta: isTouching ? this._delta : 0
     });
 
     if (isTouching) {
@@ -205,14 +209,15 @@ class Karuseru extends Component {
     let nextSlide = slide;
     const clearance = childrenWidths[slide] * 0.25;
 
-    const list = this._delta < 0
-      ? childrenWidths.slice(slide)
-      : childrenWidths.slice(0, slide);
+    const list =
+      this._delta < 0
+        ? childrenWidths.slice(slide)
+        : childrenWidths.slice(0, slide);
 
     let x = 0,
       moved = 0,
       child = list[moved];
-    while (Math.abs(this._delta) > (x + child * 0.25)) {
+    while (Math.abs(this._delta) > x + child * 0.25) {
       x += child;
       moved++;
     }
@@ -224,20 +229,20 @@ class Karuseru extends Component {
     }
 
     this.goTo(nextSlide);
-  }
+  };
 
   /**
    * Goes to the specified slide.
    *
    * @param {number} n
    */
-  goTo = (n) => {
+  goTo = n => {
     const { onChange } = this.props;
     const length = this.length;
 
     const slide = clamp(n, 0, this.length - 1);
     onChange({ slide, length });
-  }
+  };
 
   /**
    * Goes to the next slide.
@@ -246,7 +251,7 @@ class Karuseru extends Component {
     const { slide } = this.props;
 
     this.goTo(slide + 1);
-  }
+  };
 
   /**
    * Goes to the previous slide.
@@ -255,61 +260,69 @@ class Karuseru extends Component {
     const { slide } = this.props;
 
     this.goTo(slide - 1);
-  }
+  };
 
   /**
    * Convinience method for attaching event handlers.
    */
   addEventListeners = () => {
     // mouse events
-    document.addEventListener('mousemove', this.onMove);
-    document.addEventListener('mouseup', this.onEnd);
+    document.addEventListener("mousemove", this.onMove);
+    document.addEventListener("mouseup", this.onEnd);
     // touch events
-    document.addEventListener('touchmove', this.onMove);
-    document.addEventListener('touchend', this.onEnd);
-    document.addEventListener('touchcancel', this.onEnd);
-  }
+    document.addEventListener("touchmove", this.onMove);
+    document.addEventListener("touchend", this.onEnd);
+    document.addEventListener("touchcancel", this.onEnd);
+  };
 
   /**
    * Convinience method for dettaching event handlers.
    */
   removeEventListeners = () => {
     // mouse events
-    document.removeEventListener('mousemove', this.onMove);
-    document.removeEventListener('mouseup', this.onEnd);
+    document.removeEventListener("mousemove", this.onMove);
+    document.removeEventListener("mouseup", this.onEnd);
     // touch events
-    document.removeEventListener('touchmove', this.onMove);
-    document.removeEventListener('touchend', this.onEnd);
-    document.removeEventListener('touchcancel', this.onEnd);
-  }
+    document.removeEventListener("touchmove", this.onMove);
+    document.removeEventListener("touchend", this.onEnd);
+    document.removeEventListener("touchcancel", this.onEnd);
+  };
 
   render() {
     const { isTouching } = this.state;
-    const { children, slide, disabled, className, style, settings, ...props } = this.props;
+    const {
+      children,
+      slide,
+      disabled,
+      className,
+      style,
+      settings,
+      ...props
+    } = this.props;
     const { duration, easing, delay } = { ...Karuseru.SETTINGS, ...settings };
 
     const slides = React.Children.map(children, (child, i) =>
       cloneElement(child, {
         className: classnames({
           [child.props.className]: child.props.className,
-          "active": i === slide,
-          "prev": i === slide - 1,
-          "next": i === slide + 1,
+          active: i === slide,
+          prev: i === slide - 1,
+          next: i === slide + 1
         })
       })
     );
 
     const transition = !isTouching
       ? `transform ${duration}ms ${easing} ${delay}ms`
-      : '';
+      : "";
 
     const eventHandlers = disabled
       ? {}
       : {
-        onMouseDown: this.onStart,
-        onTouchStart: this.onStart,
-        onClick: this.onClick
-      };
+          onMouseDown: this.onStart,
+          onTouchStart: this.onStart,
+          onClick: this.onClick
+        };
 
     return (
       <div
@@ -318,7 +331,7 @@ class Karuseru extends Component {
         style={{
           ...style,
           transform: `translateX(${-this.currentX}px)`,
-          transition,
+          transition
         }}
         {...eventHandlers}
         {...props}
@@ -329,4 +342,5 @@ class Karuseru extends Component {
   }
 }
 
-export default Karuseru;
+// export default Karuseru;
+export default KaruseruContainer;
