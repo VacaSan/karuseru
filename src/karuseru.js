0c7a4356c0baa50971492262757506d878a4cb3d
VKotarac <vladan.kotarac@odt.net>
2020-05-05 21:33:28 +0800

    Add button support

--- src/karuseru.js
+++ src/karuseru.js
@@ -14,62 +14,52 @@ import { useDrag } from "react-use-gesture";
 
 const KaruseruContext = React.createContext(undefined);
 
-// function useKaruseru() {
-//   const context = React.useContext(KaruseruContext);
-//   if (process.env.NODE_ENV === "development" && context === undefined) {
-//     throw new Error("useKaruseru must be used inside KaruseruContext");
-//   }
-//   return context;
-// }
-
 function Karuseru({ children }) {
+  const trackRef = React.useRef(null);
+  const stopsRef = React.useRef(null);
+
+  // only state?
+  const [{ x }, set] = useVelocityTrackedSpring(() => ({ x: 0 }));
+
+  const context = React.useMemo(
+    () => ({
+      x,
+      set,
+      stopsRef,
+      trackRef,
+    }),
+    [set, x]
+  );
+
   return (
-    <KaruseruContext.Provider value={{}}>{children}</KaruseruContext.Provider>
+    <KaruseruContext.Provider value={context}>
+      {children}
+    </KaruseruContext.Provider>
   );
 }
 
-function useOnStopsChanged(onChange, [align, children]) {
-  const trackRef = React.useRef(null);
-  const prevStopsRef = React.useRef(undefined);
-  const nextStopsRef = React.useRef(undefined);
+function Track({ children, align = "center", style = {}, ...props }) {
+  const { trackRef, set, stopsRef, x } = React.useContext(KaruseruContext);
 
   // TODO update on resize...
   React.useLayoutEffect(() => {
     if (trackRef.current) {
-      prevStopsRef.current = nextStopsRef.current;
-      nextStopsRef.current = makeStops(trackRef.current, align);
-      onChange(nextStopsRef.current, prevStopsRef.current);
-    }
-  }, [align, children, onChange]);
+      const nextStops = makeStops(trackRef.current, align);
 
-  return trackRef;
-}
-
-function Track({ children, align = "center", style = {}, ...props }) {
-  // TODO const [stops, setStops] = useStops();
-  // TODO const [x, set] = useX();
-  const [stops, setStops] = React.useState([]);
-  const [{ x }, set] = useVelocityTrackedSpring(() => ({ x: 0 }));
-
-  const onStopsChanged = React.useCallback(
-    (nextStops, prevStops) => {
-      if (prevStops) {
-        const prevX = findClosestMatch(prevStops, x.getValue());
-        const index = prevStops.indexOf(prevX);
-        set({ x: nextStops[index] });
-        setStops(nextStops);
+      if (stopsRef.current) {
+        const prevX = findClosestMatch(stopsRef.current, x.getValue());
+        const index = stopsRef.current.indexOf(prevX);
+        set({ x: nextStops[index], immediate: false });
       } else {
         // TODO nextStops[initialIndex]?
         // technically i think it is safe to pass initialIndex,
         // because this branch will only fire on initial render?
         set({ x: nextStops[0], immediate: true });
-        setStops(nextStops);
       }
-    },
-    [setStops, set, x]
-  );
 
-  const trackRef = useOnStopsChanged(onStopsChanged, [align, children]);
+      stopsRef.current = nextStops;
+    }
+  }, [align, children, set, stopsRef, trackRef, x]);
 
   const bind = useDrag(
     ({ last, movement: [movementX], vxvy: [velocityX], memo }) => {
@@ -80,11 +70,11 @@ function Track({ children, align = "center", style = {}, ...props }) {
       let newX;
       if (last) {
         const projectedX = x.getValue() + projection(velocityX, 0.99);
-        newX = findClosestMatch(stops, projectedX);
+        newX = findClosestMatch(stopsRef.current, projectedX);
       } else {
         newX = rubberBandIfOutOfBounds(
-          stops[stops.length - 1],
-          stops[0],
+          stopsRef.current[stopsRef.current.length - 1],
+          stopsRef.current[0],
           memo + movementX
         );
       }
@@ -116,11 +106,30 @@ function Slide(props) {
 }
 
 function Next({ onClick, ...props }) {
-  return <button onClick={callAll(onClick)} {...props} />;
+  const { set, stopsRef, x } = React.useContext(KaruseruContext);
+
+  const next = React.useCallback(() => {
+    const currentX = findClosestMatch(stopsRef.current, x.getValue());
+    const index = stopsRef.current.indexOf(currentX);
+    const nextX =
+      stopsRef.current[Math.min(stopsRef.current.length - 1, index + 1)];
+    set({ x: nextX, immediate: false });
+  }, [set, stopsRef, x]);
+
+  return <button onClick={callAll(next, onClick)} {...props} />;
 }
 
 function Prev({ onClick, ...props }) {
-  return <button onClick={callAll(onClick)} {...props} />;
+  const { set, stopsRef, x } = React.useContext(KaruseruContext);
+
+  const prev = React.useCallback(() => {
+    const currentX = findClosestMatch(stopsRef.current, x.getValue());
+    const index = stopsRef.current.indexOf(currentX);
+    const nextX = stopsRef.current[Math.max(0, index - 1)];
+    set({ x: nextX, immediate: false });
+  }, [set, stopsRef, x]);
+
+  return <button onClick={callAll(prev, onClick)} {...props} />;
 }
 
 Karuseru.Track = Track;
